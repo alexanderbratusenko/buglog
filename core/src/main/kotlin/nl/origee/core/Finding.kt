5c483ec82c4c1f04cb12eae9135a68253fed6371
Alexander Bratusenko <zagen@bk.ru>
2021-01-08 20:42:31 +0300

    Initial gradle project with core and standalone application modules

---
+++ core/src/main/kotlin/nl/origee/core/Finding.kt
@@ -0,0 +1,7 @@
+package nl.origee.core
+
+data class Finding(
+    val type: String,
+    val cause: String,
+    val fix:String
+)
---
+++ standalone/src/main/kotlin/nl/origee/standalone/Application.kt
@@ -0,0 +1,11 @@
+@file:JvmName("Application")
+
+package nl.origee.standalone
+
+import nl.origee.core.Finding
+
+
+fun main() {
+    val finding = Finding("ihhe", "dont know", "Put")
+    println("Hello world my friend $finding")
+}
